0141c8cd56c9e0fb193f88b23fa9b1c2c645ca38
Filip Loster <fblost@gmail.com>
2015-12-30 13:53:17 +0100

    Update Help.pm

--- XMLTranslator/Modules/Help.pm
+++ XMLTranslator/Modules/Help.pm
@@ -18,6 +18,17 @@ sub showHelp {
 ****************************
 
 Tool for converting xml files to csv files for easy collaboration with translators.
+Converting xml file to csv requires creating .info file with informations about the structure of the xml document. .info file consists of searate lines of data, each line describes path to the nested node in xml file that you wish to export. For example given the xml file:
+
+<root>
+    <line>test</line>
+</root>
+
+If one wish to export content of the 'line' node .info file should contain one line:
+
+root.line
+
+The default name of the .info file is translate.info but you can specify custom one using command line argument.
 
 Arguments: [-h --help] [-c --convert s s] [-l --language s] [-i --info s]
 where:
